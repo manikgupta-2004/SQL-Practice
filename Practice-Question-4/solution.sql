ef4153e2777fe094c3e88cc8b7c7cc960391f7b9
Manik Gupta <gmanik94000@gmail.com>
2026-08-19 07:13:34 +0530

    Change LEFT JOIN to RIGHT JOIN in SQL query

--- Practice-Question-4/solution.sql
+++ Practice-Question-4/solution.sql
@@ -13,6 +13,6 @@ WHERE orders.customer_id IS NULL;
 ---Right Exclusive Join
 SELECT *
 FROM customers
-LEFT JOIN orders
+RIGHT JOIN orders
 ON customers.customer_id = orders.customer_id
 WHERE customers.customer_id IS NULL;
